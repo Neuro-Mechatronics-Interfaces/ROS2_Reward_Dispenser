6daff48905a97cf62f35e0f85524778e9bf6646c
Jonathan Shulgach <45723019+Jshulgach@users.noreply.github.com>
2023-03-15 17:45:11 -0400

    added some launch files with example of passing parameters

---
+++ reward_dispenser/launch/start_reward_dispenser.launch.py
@@ -0,0 +1,37 @@
+"""
+
+Relevant links:
+
+* https://docs.ros.org/en/galactic/Tutorials/Launch-system.html
+* https://docs.ros.org/en/galactic/Tutorials/Launch-Files/Creating-Launch-Files.html
+* https://docs.ros.org/en/galactic/Guides/Launch-file-different-formats.html
+* https://github.com/ros2/launch/blob/master/launch/doc/source/architecture.rst
+"""
+
+# Copyright 2023 Jonathan Shulgach
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.actions import ExecuteProcess
+
+def generate_launch_description():
+    
+    # Set the ROS2 domain space
+    ros_domain = ExecuteProcess( cmd=["set ROS_DOMAIN_ID=42"], shell=True, output="screen" )
+
+
+    # Reward dispenser node
+    reward_node = Node(package='reward_dispenser',
+                       namespace='reward',
+                       executable='reward',
+                       parameters=[{'config/reward.yaml'}]
+                       )
+
+    
+    return LaunchDescription([ros_domain, 
+                              reward_node,
+                              ])
